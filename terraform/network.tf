b21e04261b81d495a3b31aaad3a6c45090bf87e0
Dr4il <drail901@gmail.com>
2021-04-08 22:29:48 +0200

    used vpc module

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,27 +1,18 @@
-resource "aws_vpc" "cluster_vpc" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-  tags = var.tags
-}
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
 
-resource "aws_subnet" "subnet_a" {
-  vpc_id     = aws_vpc.cluster_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone_id = "euw1-az1"
-  tags = var.tags
-}
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-resource "aws_subnet" "subnet_b" {
-  vpc_id     = aws_vpc.cluster_vpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone_id = "euw1-az2"
-  tags = var.tags
-}
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
-resource "aws_subnet" "subnet_c" {
-  vpc_id     = aws_vpc.cluster_vpc.id
-  cidr_block = "10.0.3.0/24"
-  availability_zone_id = "euw1-az3"
-  tags = var.tags
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
